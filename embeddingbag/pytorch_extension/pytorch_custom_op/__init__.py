88a8439a052539fc20e832c5105aa26bb30c503a
Toofu <yunjae408@kaist.ac.kr>
2025-01-13 10:04:44 +0900

    add embeddingbag tpc kernel

---
+++ embeddingbag/pytorch_extension/pytorch_custom_op/__init__.py
@@ -0,0 +1,8 @@
+###############################################################################
+# Copyright (C) 2020-2023 Habana Labs, Ltd. an Intel Company
+###############################################################################
+
+from .custom_tpc import CustomHashBucket
+
+__all__ = [CustomHashBucket]
+
